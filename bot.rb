3d073681d88801dcefc97869b7a9dde92ef29686
Valov <valov.mikhail.a@gmail.com>
2023-03-15 02:23:16 +0300

    Fix

--- bot.rb
+++ bot.rb
@@ -24,6 +24,8 @@ end
 Telegram::Bot::Client.run(TG_TOKEN, environment: TG_ENV) do |bot|
   bot.listen do |message|
     bot.api.send_message(chat_id: message.chat.id, text:  handle_message(message: message.text, user_id: message.chat.id.to_s))
+  rescue
+    bot.api.send_message(chat_id: message.chat.id, text: 'Oops something is wrong, please try later')
   end
 end
 
