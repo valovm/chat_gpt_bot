3b6fd3e4099261086c1bbbcb0805b3ed9898db40
Valov <valov.mikhail.a@gmail.com>
2023-03-15 02:47:16 +0300

    Fix

--- bot.rb
+++ bot.rb
@@ -15,7 +15,7 @@ def handle_message(message:, user_id:)
     temperature: 0.7,
     user: user_id,
   })
-
+  p response
   response.dig("choices", 0, "message", "content")
 rescue
   'Oops something is wrong, please try later'
@@ -23,6 +23,7 @@ end
 
 Telegram::Bot::Client.run(TG_TOKEN, environment: TG_ENV) do |bot|
   bot.listen do |message|
+    p message
     case message
     when Telegram::Bot::Types::Message
       bot.api.send_message(chat_id: message.chat.id, text:  handle_message(message: message.text, user_id: message.chat.id.to_s))
